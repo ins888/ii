020b9043eb444dcff80c1193bedc9a6caf4e940e
ins888 <73679447+ins888@users.noreply.github.com>
2020-10-30 10:55:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <h1>พงศธร ภัทรศรีเวชการ เต้</h1>
         <p>63070110</p>
         <img src="cat.jpg">
-        <img src="jinji.jpg">
+        <img src="joji.jpg">
         <br>
         <a href='https://www.netflix.com/th/'>This is a Link</a>
 
