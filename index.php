c72c7b3b9abb62c4f195378eaee95009eb928ab3
ins888 <73679447+ins888@users.noreply.github.com>
2020-10-30 10:35:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello World<br>";
-?>
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <h1>พงศธร ภัทรศรีเวชการ เต้</h1>
+        <p>63070110</p>
+        <img src="cat.jpg">
+        <img src="wonder.jpg">
+        <br>
+        <a href='https://www.netflix.com/th/'>This is a Link</a>
+
+        <h2>The unordered list</h2>
+        <ul>
+            <li>Google</li>
+            <li>Yahoo</li>
+            <li>Amazon</li>
+        </ul>
+        
+    </body>
+</html>
