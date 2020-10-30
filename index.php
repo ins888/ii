20bf3709e129239d0469dc558517a5ed47c23384
ins888 <73679447+ins888@users.noreply.github.com>
2020-10-30 10:53:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,14 @@
         <h1>พงศธร ภัทรศรีเวชการ เต้</h1>
         <p>63070110</p>
         <img src="cat.jpg">
-        <img src="wonder.jpg">
+        <img src="jinji.jpg">
         <br>
         <a href='https://www.netflix.com/th/'>This is a Link</a>
 
-        <h2>The unordered list</h2>
-        <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
-        </ul>
-        
+        <h2>เรื่องตลกมั้กมั้ง</h2>
+        <p>คริสมาต์มีแค่วันเดียวแต่มีเธอคนเดียวทุกวัน ติ่งโป๊ะ</p>
+        <p>ไอแพทก็มีแล้ว ไอโฟนก๋มีแล้ว แล้วเมื่อไหฃร่จะมีไออุ่นจากเธอ เย้ย้ย</p>
+        <p>วันพระต้องไหว้พระ ยั้กไว้แม่เธอต้องทำไง แหะแหะๆๆ</p>
+  
     </body>
 </html>
